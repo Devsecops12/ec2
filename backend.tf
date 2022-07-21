9304e4f6e5c17129f4aab2a4a3b3764b33989f6a
Devsecops12 <108657673+Devsecops12@users.noreply.github.com>
2022-07-21 12:16:42 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    #This will allow you to download and view your state file.
+    #acl     = "bucket-owner-full-control"
+    #---------------------------
+    # NEVER CHANGE THESE VALUES
+    #---------------------------
+    #This is the bucket where to store your state file.
+    #bucket  = var.bucket_state_file
+    #This ensures the state file is stored encrypted at rest in S3.
+    encrypt = true
+    #This is the region of your S3 Bucket.
+    region  = "us-east-1"
+    #---------------------------
+    # Configurable Options
+    #---------------------------
+    #This will be the state file's file name.
+    #key     = "s3/s3-templates"
+    #This will be used as a folder in which to store your state file.
+    workspace_key_prefix = "example-ec2"
+  }
+}
